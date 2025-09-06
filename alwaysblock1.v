b517ad4ddbc8c6a62106f912ed7e8a31884c1ce8
barbarossa12 <saratheedewmith325@gmail.com>
2025-09-06 19:54:01 +1000

    Complted alwaysblock1.v of hdlbits.

---
+++ alwaysblock1.v
@@ -0,0 +1,17 @@
+/*
+* HDLBits Alwaysblock1
+* https://hdlbits.01xz.net/wiki/Alwaysblock1
+*/
+
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    assign out_assign = a & b;
+    always@(*) begin
+        out_alwaysblock = a & b;
+    end
+endmodule
+
