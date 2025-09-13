933677d3c79305e3fc9bfbee83c8e5f64f480d16
barbarossa12 <saratheedewmith325@gmail.com>
2025-09-13 16:57:06 +1000

    Completed a few of the questions.

---
+++ sequential-logic/latches-and-flipflops/exam-m2014-q4b.v
@@ -0,0 +1,13 @@
+// https://hdlbits.01xz.net/wiki/Exams/m2014_q4b
+module top_module (
+    input clk,
+    input d, 
+    input ar,   // asynchronous reset
+    output q);
+
+    always@(posedge clk or posedge ar )begin
+        if(ar == 1'b1) q <= 1'b0;
+        else
+            q <= d;
+    end
+endmodule
---
+++ sequential-logic/latches-and-flipflops/exam-m2014-q4c.v
@@ -0,0 +1,16 @@
+// https://hdlbits.01xz.net/wiki/Exams/m2014_q4c
+module top_module (
+    input clk,
+    input d, 
+    input r,   // synchronous reset
+    output q);
+
+    always@(posedge clk) begin
+        if (r == 1'b1)
+            q <= 1'b0;
+        else
+            q <= d;
+    end
+
+endmodule
+
